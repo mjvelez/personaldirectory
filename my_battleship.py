fa89181f73913735288ba3060624804bb039465c
mjvelez <mvelez@roamba.com>
2014-11-20 21:11:06 -0800

    My BattleShip!
    Version: 1.2
    Program: my_battleship

--- my_battleship.py
+++ my_battleship.py
@@ -16,7 +16,7 @@ board = []
 
 #Populate the board with rows of "O" to act as the grid of 5 x 5
     #Use a for in range loop
-for rows in range(0,5):
+for rows in range(0, 5):
     board.append(["O"] * 5)
 
 #Function that will print the blank board
@@ -27,10 +27,38 @@ def print_board(rows):
 #Create random variables and hide battleships
 from random import randint
 
-random_row = randint(0,5)
-random_col = randint(0,5)
+random_row = randint(0, 4)
+random_col = randint(0, 4)
 
 board[random_row][random_col] = "X"
+
+#Target location for debugging
+print random_row
+print random_col
+
+#Guess where the battleships are
+def guess_entry():
+    turns = 3
+    while turns > 0:
+        guess_row = int(raw_input("Guess the row of the battleship: "))
+        guess_col = int(raw_input("Guess the col of the battleship: "))
+
+        if guess_row < 5 and guess_row >= 0 and guess_col < 5 and guess_col >= 0:
+            hit = board[guess_row][guess_col]
+            print hit
+            if hit == "X":
+                print "Arrrgggg, Ye sunk me battleship...."
+                break
+            else:
+                print "Ye Missed! Try again ye scurvey devil ye!"
+                turns -= 1
+                if turns == 0:
+                    print "Thee seas be mine! Arrgggggg!!!"
+        else:
+            print "That's not on the board... Let's start this turn over."
+
+
+guess_entry()
 
 #Print the results to check the progress!!
 print_board(board)
